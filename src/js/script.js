037a311b62f9149d420d559d25d21f1108c0e2d3
Wellington <welltecnic@hotmail.com>
2024-05-02 11:02:23 -0300

    Criando a função adicionar tarefa

--- src/js/script.js
+++ src/js/script.js
@@ -10,4 +10,23 @@ function validaCampo()
     valida =true;
     return valida;
 }
+//Função que adiciona a tarefa
+
+function adicionarTarefa(){
+        //criando a variavel que vai receber as tarefas
+    let linhas =document.getElementById("task");
+
+    if(validaCampo()){
+        alert("Preencha o campo com a tarefa")
+    }else{
+        //adicionando as tarefas
+        tarefas.push(linhas.value);
+        //passando para a proxima linha vazia
+        linhas.value="";
+        //chamando a função que vai mostrar todas as tarefas
+        listarTarefas();
+    }
+    //volta para o input da tarefa
+    document.getElementById("task").focus();
+}
 
